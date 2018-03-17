a867241aea14970cf92cba1a2f290bdba97f27ce
Bhargav Sindhanuru <bhargav.s.s9876@gmail.com>
2018-03-18 02:43:08 +0530

    Added the insertEdge method

--- Graphs.py
+++ Graphs.py
@@ -18,3 +18,17 @@ class Graph:
         newNode = Node(newNodeValue)
         self.nodes.append(newNode)
 
+    def listOfNodeValues(self):
+        return [i.value for i in self.nodes]
+
+
+    def insertEdge(self, newedgeValue, nodeFromValue, nodeToValue):
+        for node in self.nodes:
+            if nodeFromValue == node.value:
+                nodeFrom = node
+            if nodeToValue == node.value:
+                nodeTo = node
+            newEdge = Edge(newedgeValue, nodeFrom, nodeTo)
+            self.edges.append(newEdge)
+            nodeFrom.edges.append(newEdge)
+            nodeTo.edges.append(newEdge)
